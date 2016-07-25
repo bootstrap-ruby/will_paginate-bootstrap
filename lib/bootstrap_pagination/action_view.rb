03fc9cc6fef311f2a4eba50ac40fe568c7bacafe
Alexey Pokhozhaev <poporul@gmail.com>
2016-07-25 15:19:01 +0300

    Add defined? check

--- lib/bootstrap_pagination/action_view.rb
+++ lib/bootstrap_pagination/action_view.rb
@@ -2,7 +2,9 @@ require "will_paginate/view_helpers/action_view"
 require "bootstrap_pagination/bootstrap_renderer"
 
 # TODO: Remove when will be fixed in will-paginate
-require "bootstrap_pagination/fix_symbolize_update"
+if defined?(ActionController::Parameters)
+  require "bootstrap_pagination/fix_symbolize_update"
+end
 
 module BootstrapPagination
   # A custom renderer class for WillPaginate that produces markup suitable for use with Twitter Bootstrap.
